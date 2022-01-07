0b08e5475b8c73c99b76b06c013a61aad06c1fa7
Eom <bonjenny1@gmail.com>
2022-01-07 21:11:29 +0900

    1/7

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-import math
-n = int(input())
-for i in range(1, n):
-    print(i, ':', math.floor((math.sqrt(8*i-3)-1)/2)+1)
+n=int(input()); lst=[]; sum=0
+for i in range(n):
+    k=int(input())
+    if k==0: lst.pop()
+    else: lst.append(k)
+for i in lst:
+    sum+=i
+print(sum)
+        
+'''
+t=int(input())
+for _ in range(t):
+    s=0; k=int(input()); n=int(input())
+    for ki in range(k):
+        if ki == 1: s += n*(n+1)/2
+        elif ki == 2: s += n*(n+1)*(2n+1)
+    print(s)
+'''
