1174d49e00ea727f19eb66deaad6d84662fb3083
woogi <bonjenny1@gmail.com>
2022-01-13 09:30:49 +0900

    1/13

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print('hello')
+n=int(input()); l=[2,7]
+if n==1: print(2)
+elif n==2: print(7)
+elif n>2:
+    for i in range(2,n):
+        l.append(2*l[i-2]+3*l[i-1])
+    print(l[n-1]%1000000007)
