3e19bb70b89acad2697cb85c1e69f6336cdb24f4
woogi <bonjenny1@gmail.com>
2022-01-19 09:27:41 +0900

    1/19

--- test.py
+++ test.py
@@ -1,42 +1,11 @@
-n,m=map(int,input().split())
-lst=[]; max=0; count=0
-
-for _ in range(n): #입력받기
-    lst.append(list(map(int,input().split())))
-
-def alst(i,j,mi,mj,l):
-    if j>0 and j-1!=mj: l.append([i,j-1])
-    if j<m-1 and j+1!=mj: l.append([i,j+1])
-    if i>0 and i-1!=mi: l.append([i-1,j])
-    if i<n-1 and i+1!=mi: l.append([i+1,j])
-
-def fmax(l):
-    max=0
-    for i,j in l:
-        if lst[i][j]>max:
-            max=lst[i][j]
-    return max
-
-for i in range(n): #max찾기
-    for j in range(m):
-        if lst[i][j]>max:
-            max=lst[i][j]
-
-for mi in range(n): #1. max 안에서
-    for mj in range(m):
-        if lst[mi][mj]==max:
-            nml=[]; alst(mi,mj,mi,mj,nml) # nmax 찾기
-            
-            for ni,nj in nml: #2. nmax 안에서
-                if lst[ni][nj]==fmax(nml):
-                    nnml=[]; alst(ni,nj,mi,mj,nnml) # nnmax 찾기
-                    
-                    for nni,nnj in nnml: #3. nnmax 안에서
-                        if lst[nni][nnj]==fmax(nnml):
-                            nnnml=[]; alst(nni,nnj,ni,nj,nnnml) # nnnmax 찾기
-                            
-                            for nnni,nnnj in nnnml: #4. nnnmax 안에서
-                                if lst[nnni][nnnj]==fmax(nnnml):
-                                    sum=lst[mi][mj]+lst[ni][nj]+lst[nni][nnj]+lst[nnni][nnnj]
-                                    if sum>count: count=sum
-print(count)
+num=int(input());
+for n in range(1,num+1):
+    l=[]; sum=n; i=n
+    while i!=0:
+        l.append(i%10);
+        i//=10
+    for i in range(len(l)): sum+=l[i]
+    if num==1: c=0
+    elif sum==num:
+        c=1; print(n); break
+if c==0: print(0)
