39e003764a7a481dee09b7ec3e76feb026e3b945
woogi <bonjenny1@gmail.com>
2022-01-20 09:34:45 +0900

    1/20

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
-num=int(input());
-for n in range(1,num+1):
-    l=[]; sum=n; i=n
-    while i!=0:
-        l.append(i%10);
-        i//=10
-    for i in range(len(l)): sum+=l[i]
-    if num==1: c=0
-    elif sum==num:
-        c=1; print(n); break
-if c==0: print(0)
+n=int(input())
+nl=[int(i) for i in input().split()]
+m=int(input())
+ml=[int(i) for i in input().split()]
+for mi in ml:
+    c=0
+    for ni in nl:
+        if mi==ni: print(1); c=1; break
+    if c==0: print(0)
