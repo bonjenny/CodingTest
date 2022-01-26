286bed71fa5fb0116a43ccddbb785248b7cc5396
woogi <bonjenny1@gmail.com>
2022-01-26 09:45:45 +0900

    1/26

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
-n,k=map(int,input().split());
-l=[i for i in range(1,n+1)]
-d=[]; cd=k
-for _ in range(n):
-    if len(l)>1: c=cd-1
-    else: c=0
-    print('c:',c)
-    d.append(l[c])
-    l.remove(l[c])
-    print(l)
-    c+=k
-    if c!=len(l) and len(l)>1: c%=len(l)
-    cd=c
-print('<',end='')
-for i in d:
-    print(i,end='')
-    if i!=d[len(d)-1]: print(',',end=' ')
-print('>',end='')
+from collections import deque
+t=int(input())
+for _ in range(t):
+    n,m=map(int,input().split())
+    l=deque([[int(i),0] for i in input().split()])
+    for i in range(len(l)):
+        l[i][1]=i
+    cnt=0
+    while True:
+        max=0
+        for i in range(len(l)):
+            if l[i][0]>max: max=l[i][0]
+        if l[0][0]<max: l.rotate(-1)
+        else:
+            cnt+=1
+            if l[0][1]==m: break
+            l.popleft()
+    print(cnt)
