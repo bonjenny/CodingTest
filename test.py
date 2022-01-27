f3d97f7da41f1c1e2d2fbb6c928933999219f3a9
woogi <bonjenny1@gmail.com>
2022-01-27 14:39:40 +0900

    1/27

--- test.py
+++ test.py
@@ -1,56 +1,23 @@
-n=int(input())-1
-ans=0
-if n//3619>0:
-    ans+=1_000_000*(n//3619)
-    n%=3619
-if n//280>0:
-    if n//280 <= 6:
-        ans+=100_000*(n//280)
-        n%=280
-    else:
-        ans+=100_000*6
-        n-=280*6
-        if n <= 65:
-            ans+=1_000*n+666
-            n=-1
-        elif n > 65 and n <= 1065:
-            ans+=66_000+n-66
-            n=-1
-        elif n > 1065 and n <= 1098:
-            ans+=1_000*(n-999)+666
-            n=-1
-        else:
-            n-=1098
-            ans+=1_000*(n//109)
-            n%=109
-if n//19>0:
-    if n//19 <= 6:
-        ans+=10_000*(n//19)
-        n%=19
-    else:
-        ans+=10_000*6
-        n-=19*6
-        if n <= 5:
-            ans+=1_000*n+666
-            n=-1
-        elif n > 5 and n <= 105:
-            ans+=6_600+n-6
-            n=-1
-        elif n > 105 and n <= 108:
-            ans+=1_000*(n-99)+666
-            n=-1
-        else:
-            n-=108
-            ans+=10_000*(n//19)
-            n%=19
-if n>=0:
-    if n <= 5:
-        ans+=1_000*n+666
-        n=-1
-    elif n > 5 and n <= 15:
-        ans+=666_0+n-6
-        n=-1
-    elif n > 15 and n <= 18:
-        ans+=1_000*(n-9)+666
-        n=-1
+from collections import deque
+deq=deque()
+ans=deque()
+n=int(input())
+for _ in range(n):
+    ans.appendleft(int(input()))
 print(ans)
+cnt=0; inp=1
+for i in range(1,len(ans)):
+    print(ans[i-1])
+    if ans[i]-1 != ans[i-1]:
+        cnt+=1
+        for j in range(ans[i]-inp):
+            deq.appendleft(inp); inp+=1
+            print('+', deq)
+        for j in range(1,ans[i]-ans[i-1]):
+            deq.popleft()
+            print('-', deq)
+        cnt=1
+    elif ans[i]==n:
+        for j in range(n-inp+1):
+            deq.appendleft(inp); inp+=1
+            print('+', deq)
