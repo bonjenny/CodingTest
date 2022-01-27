c0bc451a6c4b20b0b920eb77d46a546e9c2e4177
woogi <bonjenny1@gmail.com>
2022-01-27 09:27:48 +0900

    1/27

--- test.py
+++ test.py
@@ -1,18 +1,15 @@
-from collections import deque
-t=int(input())
-for _ in range(t):
-    n,m=map(int,input().split())
-    l=deque([[int(i),0] for i in input().split()])
-    for i in range(len(l)):
-        l[i][1]=i
-    cnt=0
-    while True:
-        max=0
-        for i in range(len(l)):
-            if l[i][0]>max: max=l[i][0]
-        if l[0][0]<max: l.rotate(-1)
-        else:
-            cnt+=1
-            if l[0][1]==m: break
-            l.popleft()
-    print(cnt)
+n=int(input())
+ans=0
+if n//3619>0:
+    print('n은 1_000_000 이상')
+    ans+=n//3619*1_000_000
+    n%=3619
+if n//280>0:
+    print('n은 100_000 이상')
+    ans+=n//280*100_000
+    n%=280
+if n//19>0:
+    print('n은 10_000 이상')
+    ans+=n//19*10_000
+    n%=19
+print(ans)
