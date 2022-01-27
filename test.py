be32d30954bfae07d837f68c0f0ae93351fc2779
woogi <bonjenny1@gmail.com>
2022-01-27 17:26:26 +0900

    1/27

--- test.py
+++ test.py
@@ -1,23 +1,20 @@
-from collections import deque
-deq=deque()
-ans=deque()
-n=int(input())
-for _ in range(n):
-    ans.appendleft(int(input()))
-print(ans)
-cnt=0; inp=1
-for i in range(1,len(ans)):
-    print(ans[i-1])
-    if ans[i]-1 != ans[i-1]:
-        cnt+=1
-        for j in range(ans[i]-inp):
-            deq.appendleft(inp); inp+=1
-            print('+', deq)
-        for j in range(1,ans[i]-ans[i-1]):
-            deq.popleft()
-            print('-', deq)
-        cnt=1
-    elif ans[i]==n:
-        for j in range(n-inp+1):
-            deq.appendleft(inp); inp+=1
-            print('+', deq)
+import sys
+n=int(sys.stdin.readline())
+l=[int(sys.stdin.readline()) for _ in range(n)]
+l.sort()
+print(round(sum(l)/n))
+print(l[n//2])
+
+d=dict()
+for i in set(l):
+    d[i]=l.count(i)
+max=[0]
+for i in set(l):
+    if d[i]>d[max[0]]:
+        max=[i]
+    elif d[i]==d[max[0]]:
+        max.append(i)
+try: print(max[1])
+except: print(max[0])
+
+print(l[-1]-l[0])
