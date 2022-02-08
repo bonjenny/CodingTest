e7d09dacc2689fd3a73bfe6da8b2fc5e5f70f354
woogi <bonjenny1@gmail.com>
2022-02-08 15:12:40 +0900

    2/8-1654

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
 import sys
-n=int(input())
-#n=int(sys.stdin.readline())
-l=[]
-while n>0:
-    l.append(n%10)
-    n=n//10
-l.sort(reverse=True)
-for i in l:
-    print(i,end='')
+k,n=map(int,input().split())
+#k,n=map(int,sys.stdin.readline().split())
+l=[0]*k
+sum=0
+for i in range(k):
+    l[i]=int(input())
+    sum+=l[i]
+#    l[i]=int(sys.stdin.readline())
+
+def bs(start, mid, end):
+    if start >= end: return start
+    sum=0
+    mid=(start+end+1)/2
+    for i in l: sum+=i//x
+    if sum==n: return mid
+    elif sum >= k: start=mid
+    else: end=mid-1
+    bs(start, mid, end)
+
+x=sum//n
+print(bs(1,(2+x)/2),x)
