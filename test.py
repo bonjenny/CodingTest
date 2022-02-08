a0660b1178d255ef9dc26e2992a23c91ab285ba5
woogi <bonjenny1@gmail.com>
2022-02-08 11:03:43 +0900

    2/8-1

--- test.py
+++ test.py
@@ -1,25 +1,10 @@
 import sys
-num=int(sys.stdin.readline())
-l=[]; c=[0]*8001
-for _ in range(num):
-    n=int(sys.stdin.readline())
-    l.append(n)
-    c[n+4000]+=1
-l.sort()
-print(round(sum(l)/num))
-print(l[num//2])
-
-most=max(c)
-cnt=0
-if c.count(most) == 1:
-    print(c.index(most)-4000)
-else:
-    for i in c:
-        if i == most:
-            cnt += 1
-            if cnt == 1:
-                c[c.index(i)] = 0
-            elif cnt == 2:
-                print(c.index(i)-4000)
-                break
-print(l[-1]-l[0])
+n=int(input())
+#n=int(sys.stdin.readline())
+l=[]
+while n>0:
+    l.append(n%10)
+    n=n//10
+l.sort(reverse=True)
+for i in l:
+    print(i,end='')
