2bd49c72c904bdc961973b8545890a8679f5c08e
woogi <bonjenny1@gmail.com>
2022-02-09 15:55:53 +0900

    2/9-1456

--- test.py
+++ test.py
@@ -2,21 +2,20 @@ import sys
 k,n=map(int,input().split())
 #k,n=map(int,sys.stdin.readline().split())
 l=[0]*k
-sum=0
 for i in range(k):
     l[i]=int(input())
-    sum+=l[i]
 #    l[i]=int(sys.stdin.readline())
 
-def bs(start, mid, end):
-    if start >= end: return start
-    sum=0
-    mid=(start+end+1)/2
-    for i in l: sum+=i//x
-    if sum==n: return mid
-    elif sum >= k: start=mid
-    else: end=mid-1
-    bs(start, mid, end)
-
-x=sum//n
-print(bs(1,(2+x)/2),x)
+start=1
+end=max(l)
+gi
+while start <= end:
+    mid=(start+end)//2
+    value=0
+    for i in range(k):
+        value += (l[i]//mid)
+    if value >= n:
+        start = mid+1
+    else:
+        end = mid-1
+print(end)
