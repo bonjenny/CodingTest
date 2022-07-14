3ba41b77d1785b62f6de0c386784ed075a1e5578
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-07-14 11:26:02 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-
+while True:
+    ip=input()
+    if ip=='.': break
+    if ip.count('[')==ip.count(']') and \
+        ip.count('(')==ip.count(')'):
+        ip=list(ip)
+        try:
+            if ip.index('[')<=ip.index(']') and \
+                ip.index('(')<=ip.index(')'):
+                print('yes')
+            else: print('no')
+        except:
+            if ip.count('[')==0 and ip.count('(')!=0:
+                if ip.index('(')<=ip.index(')'):
+                    print('yes')
+                else: print('no')
+            elif ip.count('(')==0 and ip.count('[')!=0:
+                if ip.index('[')<=ip.index(']'):
+                    print('yes')
+                else: print('no')
+            else: print('yes')
+    else: print('no')
