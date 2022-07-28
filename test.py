98ee0fc1e233f5741b54053e1a8e9ba611e6b93c
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-07-28 17:43:24 +0900

    7/28-11729

--- test.py
+++ test.py
@@ -1,31 +1,10 @@
 n=int(input())
 
-def rcs(num):
-    if num==0: return;
-    elif num==1: 
-        for i in range(n+1-num): print('____', end='')
-        print('''"재귀함수가 뭔가요?"''')
-        for i in range(n+1-num): print('____', end='')
-        print('"재귀함수는 자기 자신을 호출하는 함수라네"')
-        for i in range(n+1-num): print('____', end='')
-        print('라고 답변하였지.')
-    else:
-        for i in range(n+1-num): print('____', end='')
-        print('''"재귀함수가 뭔가요?"''')
-        for i in range(n+1-num): print('____', end='')
-        print('''"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.''')
-        for i in range(n+1-num): print('____', end='')
-        print('마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.')
-        for i in range(n+1-num): print('____', end='')
-        print('''그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."''')
-        rcs(num-1)
-        for i in range(n+1-num): print('____', end='')
-        print('라고 답변하였지.')
+def hanoi(fr, to, ex, num):
+    if num==1: print(fr, to); return
+    hanoi(fr, ex, to, num-1)
+    print(fr, to)
+    hanoi(ex, to, fr, num-1)
 
-print('''어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.
-"재귀함수가 뭔가요?"
-"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.
-마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.
-그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."''')
-rcs(n)
-print('라고 답변하였지.')
+print(2**n-1)
+hanoi(1, 3, 2, n)
