346cb907cc44014d5c86b2e1adb05826c4ae1be8
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-08-02 09:12:04 +0900

    8/2-2749

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
-def rcs(n):
-  if n==1: return ['*']
+n=int(input()); a=1; b=1; c=0
+M=1_000_000
+P=M/10*5
 
-  Stars=rcs(n//3)
-  L=[]
+for _ in range(2, int(n%P)):
+    c=a+b; a=b; b=c
 
-  for star in Stars: L.append(star*3)
-  for star in Stars: L.append(star+' '*(n//3)+star)
-  for star in Stars: L.append(star*3)
-
-  return L
-
-N=int(input())
-print('\n'.join(rcs(N)))
+if n==1: print(1)
+elif n==2: print(1)
+else: print(c%M)
