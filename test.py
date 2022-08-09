4d9b57b8cbcb8366c127672d21a447d1ce85d334
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-08-09 20:32:38 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,46 @@
-n=int(input()); a=1; b=1; c=0
-M=1_000_000
-P=M/10*5
+import sys
+n=int(sys.stdin.readline())
 
-for _ in range(2, int(n%P)):
-    c=a+b; a=b; b=c
+il=[]
+for _ in range(n):
+    il.append(int(input()))
 
-if n==1: print(1)
-elif n==2: print(1)
-else: print(c%M)
+#과정리스트에 넣을 리스트
+nl=[i for i in range(1,n+1)]
+nn=0
+rl=[] #결과 +- 넣을 리스트
+
+#과정리스트
+l=[]
+#x만큼 +
+for i in range(1, il[0]+1):
+    l.append(nl[nn]);nl[nn]=0;nn+=1
+    rl.append('+')
+l.pop();rl.append('-')
+
+try:
+    for i in range(1, n):
+        
+        # 4->3 등 pop해야하는 경우
+        if il[i]==il[i-1]-1:
+            l.pop();rl.append('-')
+        
+        # 6등 6 나오기 전까지 push해야하는 경우
+        elif il[i]>il[i-1]:
+            for j in range(nl[nn], il[i]+1):
+                l.append(nl[nn]);nl[nn]=0;nn+=1
+                rl.append('+')
+            l.pop();rl.append('-')
+        
+        # 8까지 모든 수를 다 넣은 뒤 pop해야하는 경우
+        elif nl.count(0)==n:
+            l.pop();rl.append('-')
+        
+        #예외처리
+        else: sys.stdout.write('NO\n'); rl=[]; break;
+except: sys.stdout.write('NO\n')
+
+#결과리스트 출력
+else:
+    for i in rl:
+        sys.stdout.write(i+'\n')
