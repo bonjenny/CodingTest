9a5510fb964516efedc77ac0eb32f6045f7ff3b1
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-09-14 01:24:05 +0900

    9/14-2805 (39퍼 미해결)

--- test.py
+++ test.py
@@ -1,25 +1,23 @@
 import sys
-n=int(sys.stdin.readline()) # 총개수 받기
 
-s=[]
-op=[]
-count = 1
-temp = True
+n, m = map(int, sys.stdin.readline().split())
+l = list(map(int, sys.stdin.readline().split()))
 
-for i in range(n):
-  num = int(sys.stdin.readline()) # 숫자 받기
-  
-  while count <= num: # s에 들어가는 숫자가 num에 도달할때까지
-    s.append(count)
-    op.append('+')
-    count += 1
+low = 0
+high = max(l)
 
-  if s[-1] == num: # 맨 끝 숫자 출력
-    s.pop()
-    op.append('-')
-  else: temp = False # 이게 안되면
-    
-if temp == False: print('NO') #가짜인거고
-else:
-  for i in op:
-    print(i)
+while True:
+    h = int((low + high) / 2)
+    sum = 0
+    for i in l:
+        temp = i - h
+        if temp < 0: i = 0
+        else: sum += temp
+
+    if sum == m or low > high:
+        print(h)
+        break
+    elif sum > m:
+        low = h + 1
+    else:
+        high = h - 1
