3aaa9f2d2bf24d90f0c0863283f31fb23d823d76
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-09-13 23:22:48 +0900

    9/13-1874

--- test.py
+++ test.py
@@ -1,2 +1,25 @@
-l=[1,2,3,4,5]
-print(l[::-2])
+import sys
+n=int(sys.stdin.readline()) # 총개수 받기
+
+s=[]
+op=[]
+count = 1
+temp = True
+
+for i in range(n):
+  num = int(sys.stdin.readline()) # 숫자 받기
+  
+  while count <= num: # s에 들어가는 숫자가 num에 도달할때까지
+    s.append(count)
+    op.append('+')
+    count += 1
+
+  if s[-1] == num: # 맨 끝 숫자 출력
+    s.pop()
+    op.append('-')
+  else: temp = False # 이게 안되면
+    
+if temp == False: print('NO') #가짜인거고
+else:
+  for i in op:
+    print(i)
