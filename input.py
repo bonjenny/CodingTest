619028b471a6be8a2f822ae83cd168be13fa7642
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-09-29 18:10:33 +0900

    Update input.py

--- input.py
+++ input.py
@@ -1,9 +1,8 @@
+import sys
 a, b = map(int, input().split())
 
 list = input().split()
 list = [int(i) for i in list]
-
-import sys
 list = map(int, sys.stdin.readline().split().rstrip())
 
 print('{:0,.3f}%'.format(a))
