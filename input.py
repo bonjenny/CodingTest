0bef9b4fd8bff4b6df4545d08fa1c8c18f70d0d5
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-10-04 18:26:00 +0900

    Update input.py

--- input.py
+++ input.py
@@ -1,7 +1,8 @@
 import sys
-a, b = map(int, input().split())
 
+a, b = map(int, input().split())
 list = input().split()
+
 list = [int(i) for i in list]
 list = map(int, sys.stdin.readline().split().rstrip())
 
