f0dbf59cce72e01b9a18639edba5d1b205b542e7
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-11-14 22:10:19 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 import sys
-
 n, m = map(int, sys.stdin.readline().split())
 l = list(map(int, sys.stdin.readline().split()))
 
 low = 0
 high = max(l)
-
 
 while True:
     h = int((low + high) / 2)
