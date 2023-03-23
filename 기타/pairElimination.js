b00de9cff028b331de41d9f110772bb3da2df37c
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-03-23 16:17:12 +0900

    stack과 삼항연산자 사용으로 효율성 향상

--- 기타/pairElimination.js
+++ 기타/pairElimination.js
@@ -1,40 +1,14 @@
 function solution(s)
 {
-    let answer = -1;
-    let flag = 0;
+    const stack = [];
     
-    for (let i=0; i<s.length-1; i++) {
-        if (s[i] == s[i+1]) {
-            s = s.slice(0, i) + s.slice(i+2);
-            console.log('i:', i, s);
-            flag = 1;
-            break;
+    for (let i=0; i<s.length; i++) {
+        if (!stack.length || s[i] !== stack[stack.length-1]) {
+            stack.push(s[i]);
+        } else {
+            stack.pop();
         }
     }
-    if (flag == 0) { return 0; }
     
-    flag = 1;
-    
-    while (true) {
-        if (flag == 0) { break };
-        flag = 0;
-        for (let i=0; i<s.length-1; i++) {
-            if (s[i] == s[i+1]) {
-                s = s.slice(0, i) + s.slice(i+2);
-                console.log('i:', i, s);
-                flag = 1;
-                break;
-            }
-        }
-    }
-    console.log('최종', s);
-    
-    if (s.length == 0) {
-        answer = 1;
-    }
-    else {
-        answer = 0;
-    }
-
-    return answer;
+    return stack.length ? 0 : 1;
 }
