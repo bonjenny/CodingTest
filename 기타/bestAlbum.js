03a8f2357cc6e4aec0b55dd49a26be47b7c1d441
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-03-24 16:56:45 +0900

    베스트앨범 런타임에러

--- 기타/bestAlbum.js
+++ 기타/bestAlbum.js
@@ -1,5 +1,4 @@
 function solution(genres, plays) {
-    let answer = [];
     
     let obj = {};
     for (let i=0; i<genres.length; i++) {
@@ -8,18 +7,44 @@ function solution(genres, plays) {
         obj[genre] = obj[genre] !== undefined ?
             {
                 sum : obj[genre].sum + play,
-                index: [...obj[genre].index, i],
-                plays: [...obj[genre].plays, play]
+                index: [...obj[genre].index, i]
             } :
             {
                 sum : play,
-                index: [i],
-                plays: [play]
+                index: [i]
             };
     }
-    console.log(obj);
+    // console.log(obj);
     
+    let stack = [];
+    for (key of Object.keys(obj)) {
+        stack.push({
+            name: key,
+            ...obj[key]
+        });
+    }
+    // console.log(stack);
     
+    let sorted = stack.sort(function(a, b) {
+        return b.sum - a.sum;
+    });
+    // console.log(sorted);
     
+    let answer = [];
+    for (i=0; i<sorted.length; i++) {
+        stack = [];
+        for (key of sorted[i].index) {
+            stack.push({index: key, plays: plays[key]})
+            // console.log(stack);
+        }
+        stack = stack.sort(function(a, b) {
+            return b.plays - a.plays
+        });
+        console.log(stack);
+        for (let j=0; j<2; j++) {
+            answer.push(stack[j].index);
+        }
+        console.log(answer);
+    }
     return answer;
 }
