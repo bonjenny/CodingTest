9cc472adf6908030986991afeb597c9d12432b8e
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-03-24 16:10:41 +0900

    3/24 - Lv.3 베스트앨범

--- 기타/bestAlbum.js
+++ 기타/bestAlbum.js
@@ -3,26 +3,23 @@ function solution(genres, plays) {
     
     let obj = {};
     for (let i=0; i<genres.length; i++) {
-        let index = genres[i];
+        let genre = genres[i];
         let play = parseInt(plays[i]);
-        obj[index] = obj[index] !== undefined ? obj[index] + play :  play;
-        console.log(obj);
+        obj[genre] = obj[genre] !== undefined ?
+            {
+                sum : obj[genre].sum + play,
+                index: [...obj[genre].index, i],
+                plays: [...obj[genre].plays, play]
+            } :
+            {
+                sum : play,
+                index: [i],
+                plays: [play]
+            };
     }
+    console.log(obj);
     
-    let sorted = Object.entries(obj).sort((a, b) => b[1] - a[1]);
-    console.log(sorted);
     
-    let stack = []
-    for (let i=0; i<sorted.length; i++) {
-        let genre = sorted[i][0];
-        console.log(genre);
-        for (let j=0; j<genres.length; j++) {
-            if (genre === genres[j]) {
-                stack.push(j);
-            }
-            console.log(stack);
-        }
-    }
     
     return answer;
 }
