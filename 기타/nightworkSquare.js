b6bb049d134a573e4f09b94cb5b702a291f7fa47
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-03-25 17:12:02 +0900

    Lv.3 야근 지수 - 100%

--- 기타/nightworkSquare.js
+++ 기타/nightworkSquare.js
@@ -6,7 +6,7 @@ function solution(n, works) {
     while (n > 0) {
         if (works[works.length -1] === 0) break;
         if (works[i] < works[i+1]) i++;
-        if (works[0] > works[i]) i=0;
+        if (works[0] >= works[i]) i=0;
         if (i > works.length) { i=0; }
         works[i] -= 1;
         n--;
