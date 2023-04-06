70e58211dba2d78804c5bae2ed6bb024dca944ef
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-04-06 10:24:45 +0900

    4/6 - Lv3. 네트워크 - 30.8%

--- 기타/network.js
+++ 기타/network.js
@@ -2,29 +2,25 @@ function solution(n, computers) {
     let answer = 0;
     let stack = [];
     let s;
+    let flag;
     
     for (let i=0; i<computers.length; i++) {
         s = [i];
         for (let j=i+1; j<computers[i].length; j++) {
-            if (computers[i][j] === 1) {
-                s.push(j);
-                // console.log(i, j, s, stack);
-            } else {
-                // console.log(i, j, s, stack);
-            }
+            if (computers[i][j] === 1) { s.push(j); }
         }
         if (stack.length === 0) { stack.push(s); }
         else {
+            flag = 0;
             for (j=0; j<stack.length; j++) {
-                // console.log('비교:', 's', s, 'stack', stack, '결과', stack[j].filter(it => s.includes(it)));
-                if (stack[j].filter(it => s.includes(it)).length === 0) {
-                    stack.push(s);
-                    // console.log(j, 's', s, 'stack', stack);
-                    break;
-                } else {
+                if (stack[j].filter(it => s.includes(it)).length !== 0) {
                     stack[j] = stack[j].concat(s);
-                    // console.log('s', s, 'stack', stack);
+                    flag = 1;
+                    break;
                 }
+            }
+            if (flag === 0) {
+                stack.push(s);
             }
         }
     }
