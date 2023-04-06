23119f1bfe82bbeef9acb07681fbf6523e5f9f43
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-04-06 14:35:47 +0900

    Lv3. 네트워크, DFS, 집합 - 30.8%

--- 기타/network.js
+++ 기타/network.js
@@ -2,25 +2,32 @@ function solution(n, computers) {
     let answer = 0;
     let stack = [];
     let s;
-    let flag;
+    let temp = [];
+    let flag = [];
     
     for (let i=0; i<computers.length; i++) {
-        s = [i];
+        s = new Set([i]);
         for (let j=i+1; j<computers[i].length; j++) {
-            if (computers[i][j] === 1) { s.push(j); }
+            if (computers[i][j] === 1) { s.add(j); }
         }
-        if (stack.length === 0) { stack.push(s); }
-        else {
-            flag = 0;
-            for (j=0; j<stack.length; j++) {
-                if (stack[j].filter(it => s.includes(it)).length !== 0) {
-                    stack[j] = stack[j].concat(s);
-                    flag = 1;
-                    break;
-                }
+        
+        if (stack.length === 0) { stack.push(s); continue; }
+        flag = [];
+        
+        for (j=0; j<stack.length; j++) {
+            // console.log(i, '비교:', stack[j], s, [...stack[j]].filter(x => s.has(x)));
+            if ([...stack[j]].filter(x => s.has(x)).length !== 0) {
+                temp = [...stack[j]];
+                stack[j] = new Set([...s, ...temp]);
+                flag.push(j);
+                // console.log(flag);
             }
-            if (flag === 0) {
-                stack.push(s);
+        }
+        
+        if (flag.length === 0) { stack.push(s); }
+        else {
+            for (j=0; j<flag.legnth; j++) {
+                // console.log('얍!');
             }
         }
     }
