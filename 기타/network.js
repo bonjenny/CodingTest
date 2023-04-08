a25a094596becedbb966187cd528b840fa470a84
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-04-08 17:04:43 +0900

    4/8 - Lv3. 네트워크 - 69.2%

--- 기타/network.js
+++ 기타/network.js
@@ -1,6 +1,6 @@
 function solution(n, computers) {
     let answer = 0;
-    let i, j;
+    let i, j, flag;
     const n_stack = Array.from({length: n}, (v, i) => i);
     
     let line = [0];
@@ -16,15 +16,22 @@ function solution(n, computers) {
     let diff = n_stack.filter(x => !line.includes(x));
     if (diff.length === 0) return 1;
     
+    let a_stack = new Set();
     for (i=0; i<diff.length; i++) {
+        console.log(diff[i]);
+        flag = 0;
         for (j=0; j<computers[diff[i]].length; j++) {
             console.log(diff[i], j, computers[diff[i]]);
             if (diff[i] !== j && computers[diff[i]][j] === 1) {
                 console.log(i, j, computers[diff[i]][j]);
-                diff.push(j);
+                flag = 1;
+                if (!diff.includes(j)) diff.push(j);
             }
         }
+        if (flag === 0) a_stack.add(diff[i]);
     }
+    console.log('a_stack', a_stack);
     
+    answer = a_stack.size + 1;
     return answer;
 }
