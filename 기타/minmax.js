6442d7ebdba88ed3879b5ea6f99b5f86abdb0e04
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-04-11 19:10:10 +0900

    Delete minmax.js

--- 기타/minmax.js
+++
@@ -1,8 +0,0 @@
-function solution(s) {
-    let answer = '';
-    let array = s.split(' ').map(Number);
-    let max = Math.max.apply(null, array);
-    let min = Math.min.apply(null, array);
-    answer = min.toString() + ' ' + max.toString();
-    return answer;
-}
